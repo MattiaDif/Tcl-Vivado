d05ae1a5c9540c23efa25a76485b2029724790a5
MattiaDif <d.florio.mattia@gmail.com>
2024-06-12 15:58:41 +0200

    add -force to init script add simulation script

--- code/sim_prj.tcl
+++ code/sim_prj.tcl
@@ -2,10 +2,14 @@
 set selected_simulator XSim
 set top_module_sim "adder_tb"
 
+
+# Set module to simulate
 set_property top $top_module_sim [get_filesets sim_1]
 
+#Set target simulator
 set_property target_simulator $selected_simulator [current_project]
 
+# Lunch simulation
 launch_simulation
 
 puts "Simulation launched with $selected_simulator."
